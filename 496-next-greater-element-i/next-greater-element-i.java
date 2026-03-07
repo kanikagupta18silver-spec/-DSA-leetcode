1e1fd4771a12e5498d8e4d06efee804f434e164a
kanikagupta18silver-spec <kanikagupta18silver@gmail.com>
2026-03-07 12:09:44 +0530

    Time: 5 ms (16.46%) | Memory: 44.6 MB (97.53%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        int[] ans = new int[nums1.length] ;
+        for(int i = 0 ; i < nums1.length ; i++)
+        {
+            ans[i] = -1 ;
+        }
+        for(int i = 0 ;  i < nums1.length ; i++)
+        {
+            for(int j = 0 ; j < nums2.length ; j++)
+            {
+                if(nums1[i] == nums2[j])
+                {
+                    //next larger find karo
+                    for(int k = j+1 ; k < nums2.length ; k++)
+                    {
+                        if(nums2[j] < nums2[k])
+                        {
+                            ans[i] = nums2[k] ;
+                            break ;
+                        }
+                    }
+                }
+            }
+        }
+        return ans ;
+    }
+}
