590b1e7921b434b3e2edebff57c35592850e78cc
kanikagupta18silver-spec <kanikagupta18silver@gmail.com>
2026-04-02 12:47:38 +0530

    Time: 130 ms (73.92%) | Memory: 42.8 MB (91.19%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,36 +1,31 @@
 class Solution {
+
+    public static boolean Solve(int i , int j , int idx, char[][] board , String word)
+    {
+        if(idx == word.length()) return true ;
+
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || word.charAt(idx) != board[i][j]) return false ;
+
+        char temp = board[i][j] ;
+        board[i][j] = '#' ;
+        boolean ans = Solve(i+1,j,idx+1,board,word) || Solve(i-1,j,idx+1,board,word) || Solve(i,j+1,idx+1,board,word) || Solve(i,j-1,idx+1,board,word)  ;
+
+        board[i][j] = temp ;
+        return ans ;
+    }
     public boolean exist(char[][] board, String word) {
-        int m = board.length, n = board[0].length;
+        
+        int m = board.length ;
+        int n = board[0].length ;
 
-        int[] count = new int[128];
-        for (char[] row : board) {
-            for (char c : row) count[c]++;
-        }
-        for (char c : word.toCharArray()) {
-            if (--count[c] < 0) return false;
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dfs(board, word, i, j, 0)) return true;
+        for(int i = 0 ; i < m ; i++)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                if(Solve(i,j,0,board,word)) return true ;
             }
         }
-        return false;
-    }
 
-    private boolean dfs(char[][] board, String word, int i, int j, int k) {
-        if (k == word.length()) return true;
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(k)) return false;
-
-        char temp = board[i][j];
-        board[i][j] = '#';
-
-        boolean found = dfs(board, word, i + 1, j, k + 1) ||
-                        dfs(board, word, i - 1, j, k + 1) ||
-                        dfs(board, word, i, j + 1, k + 1) ||
-                        dfs(board, word, i, j - 1, k + 1);
-
-        board[i][j] = temp;
-        return found;
+        return false ;
     }
 }
