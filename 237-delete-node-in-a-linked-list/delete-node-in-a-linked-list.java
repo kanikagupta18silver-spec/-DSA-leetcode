f8054133e31807774293d39d7e705745530ce3c0
kanikagupta18silver-spec <kanikagupta18silver@gmail.com>
2026-04-09 12:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (12.88%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;       // next ka value copy
+        node.next = node.next.next;     // next node delete
+    }
+}
