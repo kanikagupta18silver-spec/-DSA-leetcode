557e6ec389b2bf6583ebedff41697b9ed4a7065b
Kanika Gupta <kanikagupta18silver@gmail.com>
2026-04-12 19:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (95.24%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,42 +1,27 @@
 class Solution {
-    public static int Solve(int[] nums ,int i ,int[] dp)
 
-{
+    public static int Solve(int i, int[] nums, int[] dp)
+    {
+        if(i >= nums.length)
+        {
+            return 0;
+        }
 
-if(i >= nums.length) return 0;
+        if(dp[i] != -1) return dp[i];
 
-if(dp[i] != -1)
+        int take = nums[i] + Solve(i+2, nums, dp);
+        int notTake = Solve(i+1, nums, dp);
 
-{
+        return dp[i] = Math.max(take, notTake);
+    }
 
-return dp[i];
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+        {
+            dp[i] = -1;
+        }
 
-}
-
-int take = nums[i] + Solve (nums,i+2,dp);
-
-int skip =Solve (nums, i+1,dp);
-
-dp[i] = Math.max(skip, take);
-
-return dp[i];
-
-
-
-}
-
-public int rob(int[] nums) {
-
-int n = nums.length;
-
-int[] dp = new int[n];
-
-for(int i = 0; i < n; i++)
-
-{
-
-dp[i] = -1;
-}
-return Solve(nums,0,dp);
-}
+        return Solve(0, nums, dp);
+    }
 }
